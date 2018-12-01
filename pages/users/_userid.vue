fd04356100aaac1aeca5bcdee59d5d128e6f9aaa
subta90 <haxyoy9990mk3@gmail.com>
2018-12-01 16:27:44 +0900

    first commit.

---
+++ pages/users/_userid.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="container">
+    <div>
+      <h3>{{user.id}}</h3>
+      <img :src="user.profile_image_url" width="120" alt="">
+      <p>{{user.description || 'No description'}}</p>
+      <p>
+        <nuxt-link to="/">
+          <small><b>トップへ戻る</b></small>
+        </nuxt-link>
+      </p>
+      <h3>{{user.id}}さんの投稿一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{item.title}}</span>
+          </h4>
+          <div>{{item.body.slice(0,130)}}......</div>
+          <p><a target="_blank" :href="item.url">{{item.url}}</a></p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  head() {
+    return {
+      title: this.user.id
+    }
+  },
+  async asyncData({ route, store, redirect }) {
+    if (store.getters['users'][route.params.userid]) {
+      return
+    }
+    console.log()
+    await store.dispatch('fetchUserInfo', { id: route.params.userid })
+  },
+  computed: {
+    user() {
+      return this.users[this.$route.params.userid]
+    },
+    items() {
+      return this.userItems[this.$route.params.userid] || []
+    },
+    ...mapGetters(['users', 'userItems'])
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  min-height: 100vh;
+  padding: 16px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: soslid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+
+</style>
